5131733a7309d462edec5bfb09c32e3100dd1438
Bianca <biancancanal@gmail.com>
2021-05-21 14:04:40 -0500

    Complete

--- lib/school.rb
+++ lib/school.rb
@@ -10,4 +10,24 @@ class School
         roster[grade] ||= []
         roster[grade] << student
     end 
-end 
+
+    def grade(grade)
+        roster[grade] 
+    end
+    
+    def sort
+        abc_roster = {}     
+        roster.each do |grade, students|
+            abc_roster[grade] = students.sort
+        end 
+        abc_roster
+    end 
+end
+
+
+
+
+#a ||= b is a conditional assignment operator. 
+#It means if `a` is undefined or falsey, then evaluate `b`and set `a` to the result. 
+#Equivalently, if `a` is defined and evaluates to truthy, 
+#then `b` is not evaluated, and no assignment takes place.
